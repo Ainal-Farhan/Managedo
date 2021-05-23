f2574e09f4fe9834b99f1d856f0c5f2cf3db1f22
Mohd Ainal Farhan Mohamad Johari <56626447+Ainal-Farhan@users.noreply.github.com>
2021-05-23 21:53:33 +0800

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ void main() {
       title: 'Managedo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.green),
-      initialRoute: router.exampleRoute,
+      initialRoute: router.educationRoute,
       onGenerateRoute: router.createRoute,
     ),
   );
